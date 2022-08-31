10173fcfe6dcd645ab455451fed5732a411757ed
maoko <maaaoko0021@gmail.com>
2022-09-01 06:45:28 +0900

    Initial Commit

---
+++ spmUITests/spmUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  spmUITestsLaunchTests.swift
+//  spmUITests
+//
+//  Created by Maoko Furuya on 2022/09/01.
+//
+
+import XCTest
+
+class spmUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
